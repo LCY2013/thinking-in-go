f181055c8472ec515775f205019cb956b511a0d5
fufeng <15281205719@163.com>
2020-11-11 16:37:59 +0800

    go mutex 第一版

--- concurrent/mutex/mutex-info.go
+++ concurrent/mutex/mutex-info.go
@@ -214,3 +214,31 @@ func (counter *Counter) counts() uint64 {
 	counter.mutex.Lock()
 	return counter.count
 }
+
+// 归并排序
+func mergeSort(r []int) []int {
+	length := len(r)
+	if length <= 1 {
+		return r
+	}
+	num := length / 2
+	left := mergeSort(r[:num])
+	right := mergeSort(r[num:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) (result []int) {
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+	return
+}
