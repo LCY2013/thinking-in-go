06d10ca42262ac6c8fdacf15fea7db91450959d5
fufeng <15281205719@163.com>
2020-12-04 17:20:16 +0800

    指针在go中的使用

---
+++ thewaytogo/ipoint/pointer_info.go
@@ -0,0 +1,65 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2020 fufeng.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-go
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-12-04
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+
+	pointerInfo()
+
+	pointerString()
+
+	pointerToPointer()
+
+}
+
+// panic: runtime error: invalid memory address or nil pointer dereference
+func pointerToPointer() {
+	var p *int = nil
+	*p = 0
+}
+
+func pointerString() {
+	s := "good bye"
+	var p *string = &s
+	*p = "ciao"
+	fmt.Printf("Here is the pointer p: %p\n", p)  // prints address
+	fmt.Printf("Here is the string *p: %s\n", *p) // prints string
+	fmt.Printf("Here is the string s: %s\n", s)   // prints same string
+
+	// 下列展示错误，不能得到常量或者文字的地址
+	/*const i = 5
+	//error: cannot take the address of i
+	ptr := &i
+	//error: cannot take the address of 10
+	ptr2 := &10*/
+}
+
+// 一个指针变量可以指向任何一个值的内存地址 它指向那个值的内存地址，在 32 位机器上占用 4 个字节，在 64 位机器上占用 8 个字节，
+//并且与它所指向的值的大小无关。当然，可以声明指针指向任何类型的值来表明它的原始性或结构性；
+//你可以在指针类型前面加上 号（前缀）来获取指针所指向的内容，这里的 号是一个类型更改器。使用一个指针引用一个值被称为间接引用。
+func pointerInfo() {
+	var i1 = 5
+	fmt.Printf("An integer: %d, it's location in memory: %p\n", i1, &i1)
+
+	var i2 *int
+	a := 1
+	i2 = &a
+	fmt.Printf("An integer: %d, it's location in memory: %p\n", *i2, i2)
+}
