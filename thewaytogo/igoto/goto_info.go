9388d54840c4be28b53903404448d33e27a4cbc5
fufeng <15281205719@163.com>
2020-12-07 16:34:37 +0800

    go goto

---
+++ thewaytogo/igoto/goto_info.go
@@ -0,0 +1,82 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2020 fufeng.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-go
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-12-07
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+	continueInfo()
+
+	gotoInfo()
+
+	gotoJumpErrorInfo()
+
+	forInfo()
+}
+
+func forInfo() {
+	i := 0
+	for { //since there are no checks, this is an infinite loop
+		if i >= 3 {
+			break
+		}
+		//break out of this for loop when this condition is met
+		fmt.Println("Value of i is:", i)
+		i++
+	}
+	fmt.Println("A statement just after for loop.")
+}
+
+// 如果您必须使用 goto，应当只使用正序的标签（标签位于 goto 语句之后），但注意标签和 goto 语句之间不能出现定义新变量的语句，否则会导致编译失败。
+func gotoJumpErrorInfo() {
+	/*a := 1
+	goto TARGET // compile error
+	b := 9
+	TARGET:
+	b += a
+	fmt.Printf("a is %v *** b is %v", a, b)*/
+}
+
+// 01234
+func gotoInfo() {
+	i := 0
+HERE:
+	print(i)
+	i++
+	if i == 5 {
+		fmt.Println()
+		return
+	}
+	goto HERE
+}
+
+// continue 语句指向 LABEL1，当执行到该语句的时候，就会跳转到 LABEL1 标签的位置。
+// 您可以看到当 j==4 和 j==5 的时候，没有任何输出：标签的作用对象为外部循环，因此 i 会直接变成下一个循环的值，
+//而此时 j 的值就被重设为 0，即它的初始值。如果将 continue 改为 break，则不会只退出内层循环，而是直接退出外层循环了。
+//另外，还可以使用 goto 语句和标签配合使用来模拟循环。
+func continueInfo() {
+LABEL1:
+	for i := 0; i <= 5; i++ {
+		for j := 0; j <= 5; j++ {
+			if j == 4 {
+				continue LABEL1
+			}
+			fmt.Printf("i is: %d, and j is: %d\n", i, j)
+		}
+	}
+}
