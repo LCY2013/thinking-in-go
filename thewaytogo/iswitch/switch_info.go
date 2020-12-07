8f7898d94d4911058e711f528fb3c5f64028ae1a
fufeng <15281205719@163.com>
2020-12-07 15:40:14 +0800

    go switch information

---
+++ thewaytogo/iswitch/switch_info.go
@@ -0,0 +1,84 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2020 fufeng.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-go
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-12-04
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+	switch1(1)
+
+	switch2(1)
+
+	switch3()
+
+	switch4()
+}
+
+func switch4() {
+	k := 6
+	switch k {
+	case 4:
+		fmt.Println("was <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("was <= 5")
+		fallthrough
+	case 6:
+		fmt.Println("was <= 6")
+		fallthrough
+	case 7:
+		fmt.Println("was <= 7")
+		fallthrough
+	case 8:
+		fmt.Println("was <= 8")
+		fallthrough
+	default:
+		fmt.Println("default case")
+	}
+}
+
+func switch3() {
+	var num1 int = 100
+
+	switch num1 {
+	case 98, 99:
+		fmt.Println("It's equal to 98")
+	case 100:
+		fmt.Println("It's equal to 100")
+	default:
+		fmt.Println("It's not equal to 98 or 100")
+	}
+}
+
+// 程序不会自动地去执行下一个分支的代码。如果在执行完每个分支的代码后，还希望继续执行后续分支的代码，可以使用 fallthrough 关键字来达到目的。
+func switch2(i int) {
+	switch i {
+	case 0:
+		fallthrough
+	case 1:
+		go func() {}() // 当 i == 0 时函数也会被调用
+	}
+}
+
+func switch1(i int) {
+	switch i {
+	case 0: // 空分支，只有当 i == 0 时才会进入分支
+	case 1:
+		go func() {}() // 当 i == 0 时函数不会被调用
+	}
+}
