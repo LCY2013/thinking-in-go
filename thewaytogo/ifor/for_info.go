b6c42ee0c6207aa1488d64095fcd684ccaccfdc7
fufeng <15281205719@163.com>
2020-12-07 16:14:27 +0800

    go for information

---
+++ thewaytogo/ifor/for_info.go
@@ -0,0 +1,94 @@
+/*
+ * The MIT License (MIT)
+ * ------------------------------------------------------------------
+ * Copyright © 2020 fufeng.All Rights Reserved.
+ *
+ * ProjectName: thinking-in-go
+ * @Author : <a href="https://github.com/lcy2013">MagicLuo(扶风)</a>
+ * @date : 2020-12-07
+ * @version : 1.0.0-RELEASE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+	forString()
+
+	fmt.Println()
+
+	complement()
+
+	fmt.Println()
+
+	forRange()
+
+	whileRange()
+}
+
+func whileRange() {
+	/*for i := 0; ; i++ {
+		fmt.Println("Value of i is now:", i)
+	}*/
+
+	/*for i := 0; i < 3; {
+		fmt.Println("Value of i:", i)
+	}*/
+
+	/*s := ""
+	for ; s != "aaaaa"; {
+		fmt.Println("Value of s:", s)
+		s = s + "a"
+	}*/
+
+	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j,
+		s = i+1, j+1, s+"a" {
+		fmt.Println("Value of i, j, s:", i, j, s)
+	}
+}
+
+func forRange() {
+	str := "Go is a beautiful language!"
+	fmt.Printf("The length of str is: %d\n", len(str))
+	for pos, char := range str {
+		fmt.Printf("Character on position %d is: %c \n", pos, char)
+	}
+	fmt.Println()
+	str2 := "Chinese: 中文"
+	fmt.Printf("The length of str2 is: %d\n", len(str2))
+	for pos, char := range str2 {
+		fmt.Printf("character %c starts at byte position %d\n", char, pos)
+	}
+	fmt.Println()
+	fmt.Println("index int(rune) rune    char bytes")
+	for index, rune := range str2 {
+		fmt.Printf("%-2d      %d      %U '%c' % X\n", index, rune, rune, rune, []byte(string(rune)))
+	}
+}
+
+// 使用按位补码从 0 到 10，使用位表达式 %b 来格式化输出。
+func complement() {
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("the complement of %b is: %b\n", i, ^i)
+	}
+}
+
+func forString() {
+	str := "Go is a beautiful language!"
+	fmt.Printf("The length of str is: %d\n", len(str))
+	for ix := 0; ix < len(str); ix++ {
+		fmt.Printf("Character on position %d is: %c \n", ix, str[ix])
+	}
+	str2 := "中文"
+	fmt.Printf("The length of str2 is: %d\n", len(str2))
+	for ix := 0; ix < len(str2); ix++ {
+		fmt.Printf("Character on position %d is: %c \n", ix, str2[ix])
+	}
+}
