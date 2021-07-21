4c51b40e9409f95be4bebf72907148e6af2d8808
luocy <luocy@dustess.com>
2021-07-21 20:44:57 +0800

    feat: gin http 单文件上传

---
+++ gin/upload-file/single/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	router := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("upload-file/single/public", "./upload-file/single/public")
+	router.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Source
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+
+		filename := filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, filename); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully with fields name=%s and email=%s.", file.Filename, name, email))
+	})
+	router.Run(":8080")
+}
+
+/*
+curl -X POST http://localhost:8080/upload \
+  -F "file=@/Users/magicLuoMacBook/Downloads/1.txt" \
+  -H "Content-Type: multipart/form-data" \
+  --form name=fufeng \
+  --form email=fufeng@email.com
+*/
