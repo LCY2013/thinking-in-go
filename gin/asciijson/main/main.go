efc4aa72a1c53cac238b3101c5f840ff4d441ffe
luocy <luocy@dustess.com>
2021-07-21 16:19:54 +0800

    feat: gin AsciiJSON 生成具有转义的非 ASCII 字符的 ASCII-only JSON

---
+++ gin/asciijson/main/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// main 使用 AsciiJSON 生成具有转义的非 ASCII 字符的 ASCII-only JSON
+func main() {
+	r := gin.Default()
+
+	// 定义一个相对路由
+	r.GET("/someJSON", func(context *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO语言",
+			"tag":  "</br>",
+		}
+
+		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+		context.AsciiJSON(http.StatusOK, data)
+	})
+
+	// 默认监听在 0.0.0.0:8080 上面
+	r.Run()
+}
