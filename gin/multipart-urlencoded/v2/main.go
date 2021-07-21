94c305a6c5893a5ca195a37199c191dcb3a9f7a0
luocy <luocy@dustess.com>
2021-07-21 20:05:47 +0800

    feat: gin http Multipart/Urlencoded 表单提交

---
+++ gin/multipart-urlencoded/v2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.POST("/form_post", func(context *gin.Context) {
+		message := context.PostForm("message")
+		name := context.DefaultPostForm("name", "fufeng")
+
+		context.JSON(http.StatusOK, gin.H{
+			"statu":   "posted",
+			"name":    name,
+			"message": message,
+		})
+	})
+
+	router.Run(":8080")
+}
+
+// curl -v --form message=hello --form name=lcy http://127.0.0.1:8080/form_post
+// curl -v --form message=hello http://127.0.0.1:8080/form_post
