a3624f4a13995017a942099f0b62ffe63cc213ab
luocy <luocy@dustess.com>
2021-07-21 19:50:29 +0800

    feat: gin http pusher

---
+++ gin/http-pusher/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"log"
+)
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+  <script src="http-pusher/assets/app.js"></script>
+</head>
+<body>
+  <h1 style="color:red;">Welcome, fufeng!</h1>
+</body>
+</html>
+`))
+
+func main() {
+	r := gin.Default()
+	r.Static("http-pusher/assets", "./http-pusher/assets")
+	r.SetHTMLTemplate(html)
+
+	r.GET("/", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			// 使用 pusher.Push() 做服务器推送
+			if err := pusher.Push("http-pusher/assets/app.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		c.HTML(200, "https", gin.H{
+			"status": "success",
+		})
+	})
+
+	// 监听并在 https://127.0.0.1:8080 上启动服务
+	r.RunTLS(":8080", "http-pusher/testdata/server.pem", "http-pusher/testdata/server.key")
+}
