44b212fd01dc7b9cd40244918cd55933ac81cfc7
luocy <luocy@dustess.com>
2021-07-21 19:55:02 +0800

    feat: gin http jsonp

---
+++ gin/jsonp/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/JSONP", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"foo": "bar",
+		}
+
+		// /JSONP?callback=x
+		// 将输出：x({\"foo\":\"bar\"})
+		c.JSONP(http.StatusOK, data)
+	})
+
+	// 监听并在 0.0.0.0:8080 上启动服务
+	r.Run(":8080")
+}
