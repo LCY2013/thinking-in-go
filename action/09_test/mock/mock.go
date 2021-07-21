8307c3b6f0ecf7401548bf79c436897fa178384b
luocy <luocy@dustess.com>
2021-07-21 11:36:02 +0800

    feat: go mock test

---
+++ action/09_test/mock/mock.go
@@ -0,0 +1,37 @@
+package mock
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+)
+
+const checkMark = "\u2713"
+const ballotX = "\u2717"
+
+// feed 模仿我们期望接收的 XML 文档
+var feed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss>
+<channel>
+	<title>Going Go Programming</title>
+	<description>Golang : https://github.com/goinggo</description>
+	<link>http://www.goinggo.net/</link>
+	<item>
+		<pubDate>Sun, 15 Mar 2015 15:04:00 +0000</pubDate>
+		<title>Object Oriented Programming Mechanics</title>
+		<description>o is an object oriented language.</description>
+		<link>http://www.goinggo.net/2015/03/object-oriented</link>
+	</item>
+<channel>
+<rss>
+`
+
+// mockServer 返回用来处理请求的服务器指针
+func mockServer() *httptest.Server {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/xml")
+		fmt.Fprintln(w, feed)
+	}
+	return httptest.NewServer(http.HandlerFunc(f))
+}
---
+++ action/09_test/mock/mock_test.go
@@ -0,0 +1,38 @@
+package mock
+
+import (
+	"net/http"
+	"testing"
+)
+
+// TestDownload 确认 http 包的 Get 函数可以下载内容，并且内容可以被序列化和反序列化
+// 在没有网络的情况下，通过 mock 函数执行模拟，让测试代码执行通过
+func TestDownload(t *testing.T) {
+	statusCode := http.StatusOK
+
+	server := mockServer()
+	defer server.Close()
+
+	t.Log("Given the need to test downloading content.")
+	{
+		t.Logf("\twhen checking \"%s\" for status code \"%d\"",
+			server.URL, statusCode)
+		{
+			resp, err := http.Get(server.URL)
+			if err != nil {
+				t.Fatal("\t\tshould be able to make the Get call.",
+					ballotX, err)
+			}
+			t.Log("\t\tshould be able to make the Get call.", checkMark)
+
+			defer resp.Body.Close()
+
+			if resp.StatusCode != statusCode {
+				t.Fatalf("\t\tshould receive a \"%d\" status. %v %v",
+					statusCode, ballotX, resp.StatusCode)
+			}
+			t.Logf("\t\tshould receive a \"%d\" status. %v", statusCode, checkMark)
+		}
+	}
+
+}
