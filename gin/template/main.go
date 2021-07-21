a3514934a826fa7c08ac98bb155a0916117ed365
luocy <luocy@dustess.com>
2021-07-21 17:49:02 +0800

    feat: gin template

---
+++ gin/template/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+func formatAsDate(t time.Time) string {
+	date, month, day := t.Date()
+	return fmt.Sprintf("%d%02d/%02d", date, month, day)
+}
+
+func main() {
+	router := gin.Default()
+	router.Delims("{[{", "}]}")
+	router.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+
+	router.LoadHTMLGlob("./testdata/*")
+	//router.LoadHTMLFiles("./testdata/raw.tmpl")
+
+	router.GET("/raw", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "raw.tmpl", gin.H{
+			"now": time.Date(2021, 07, 01, 0, 0, 0, 0, time.Local),
+		})
+	})
+
+	router.Run(":8080")
+}
