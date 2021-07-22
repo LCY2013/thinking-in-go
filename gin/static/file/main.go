4b340a8403a4415e85dc292cd62dddaef1384e6b
luocy <luocy@dustess.com>
2021-07-22 18:29:23 +0800

    feat: gin http 静态文件服务

---
+++ gin/static/file/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/assets", "./assets")
+	router.StaticFS("/more_static", http.Dir("my_file_system"))
+	router.StaticFile("/favicon.ico", "./resources/favicon.ico")
+
+	// 监听并在 0.0.0.0:8080 上启动服务
+	router.Run(":8080")
+}
