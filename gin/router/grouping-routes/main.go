9add263ab5dc38d0edf322c2076259c870d1d204
luocy <luocy@dustess.com>
2021-07-22 18:20:25 +0800

    feat: gin http 路由组

---
+++ gin/router/grouping-routes/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// main 路由组
+func main() {
+	router := gin.Default()
+
+	// 简单的路由组: v1
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/login", loginEndpoint)
+		v1.POST("/submit", submitEndpoint)
+		v1.POST("/read", readEndpoint)
+	}
+
+	// 简单的路由组: v2
+	v2 := router.Group("/v2")
+	{
+		v2.POST("/login", loginEndpoint)
+		v2.POST("/submit", submitEndpoint)
+		v2.POST("/read", readEndpoint)
+	}
+
+	router.Run(":8080")
+}
+
+func readEndpoint(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"path": fmt.Sprintf("read %s", context.Request.RequestURI),
+	})
+}
+
+func submitEndpoint(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"path": fmt.Sprintf("submit %s", context.Request.RequestURI),
+	})
+}
+
+func loginEndpoint(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"path": fmt.Sprintf("login %s", context.Request.RequestURI),
+	})
+}
