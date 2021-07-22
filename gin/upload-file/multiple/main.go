d368592ceaa4658006ef01900f4a7d17267c0c83
luocy <luocy@dustess.com>
2021-07-22 10:58:19 +0800

    feat: gin http 多文件上传

---
+++ gin/upload-file/multiple/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20 // 8 MiB
+	router.Static("upload-file/multiple/public", "./upload-file/multiple/public")
+	router.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		// Multipart form
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+		files := form.File["files"]
+
+		for _, file := range files {
+			filename := filepath.Base(file.Filename)
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+				return
+			}
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files with fields name=%s and email=%s.", len(files), name, email))
+	})
+
+	// swage ⽂档访问路由
+	//eng :=gin.Default()
+	//eng.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	router.Run(":8080")
+}
+
+/*
+curl -X POST http://localhost:8080/upload \
+  -F "upload[]=@/Users/magicLuoMacBook/Downloads/1.txt" \
+  -F "upload[]=@/Users/magicLuoMacBook/Downloads/2.txt" \
+  -H "Content-Type: multipart/form-data" \
+  --form name=fufeng \
+  --form email=fufeng@email.com
+*/
