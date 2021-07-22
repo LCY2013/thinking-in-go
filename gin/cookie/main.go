9ed3073e5aa61914978e72efcbbbc7ca94bd6c84
luocy <luocy@dustess.com>
2021-07-22 18:13:04 +0800

    feat: gin http 设置和获取 Cookie

---
+++ gin/cookie/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+// main 设置和获取 Cookie
+func main() {
+	router := gin.Default()
+
+	router.GET("/cookie", func(c *gin.Context) {
+
+		cookie, err := c.Cookie("gin_cookie")
+
+		if err != nil {
+			cookie = "NotSet"
+			c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+		}
+
+		fmt.Printf("Cookie value: %s \n", cookie)
+	})
+
+	router.Run()
+}
