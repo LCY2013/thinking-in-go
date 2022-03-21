0e7ad25edc509d554eb8c467d6ddef9d5b919b95
fufeng <luochunyun1995@gmail.com>
2022-03-21 09:12:29 +0800

    🎸feat: go generics

--- base/generics/go.mod
+++ base/generics/go.mod
@@ -1,3 +1,5 @@
 module github.com/lcy2013/generics
 
 go 1.18
+
+require golang.org/x/exp v0.0.0-20220318154914-8dddf5d87bd8
---
+++ base/generics/v02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
+
+// Min go 范性实现
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(Min(1, 2))
+	fmt.Println(Min(1.0, 2.0))
+}
