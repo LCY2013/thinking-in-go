b70641dd25e889f175f6610ed4989d8baf6d39d8
fufeng <luocy@dustess.com>
2022-05-07 21:29:51 +0800

    🎸 feat: go reordering

---
+++ advanced/reordering/cpu/cpu_reordering.go
@@ -0,0 +1,60 @@
+package main
+
+/*
+==== CPU 重排 ====
+litmus 验证
+
+cat sb.litmus
+
+X86 SB
+{ x=0; y=0; }
+ P0          | P1          ;
+ MOV [x],$1  | MOV [y],$1  ;
+ MOV EAX,[y] | MOV EAX,[x] ;
+locations [x;y;]
+exists (0:EAX=0 /\ 1:EAX=0)
+⇒
+
+~ ❯❯❯ bin/litmus7 ./sb.litmus
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Results for ./sb.litmus %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+X86 SB
+
+{x=0; y=0;}
+
+ P0          | P1          ;
+ MOV [x],$1  | MOV [y],$1  ;
+ MOV EAX,[y] | MOV EAX,[x] ;
+
+locations [x; y;]
+exists (0:EAX=0 /\ 1:EAX=0)
+Generated assembler
+	##START _litmus_P0
+	movl	$1, -4(%rbx,%rcx,4)
+	movl	-4(%rsi,%rcx,4), %eax
+	##START _litmus_P1
+	movl	$1, -4(%rsi,%rcx,4)
+	movl	-4(%rbx,%rcx,4), %eax
+
+Test SB Allowed
+Histogram (4 states)
+96    *>0:EAX=0; 1:EAX=0; x=1; y=1;
+499878:>0:EAX=1; 1:EAX=0; x=1; y=1;
+499862:>0:EAX=0; 1:EAX=1; x=1; y=1;
+164   :>0:EAX=1; 1:EAX=1; x=1; y=1;
+Ok
+
+Witnesses
+Positive: 96, Negative: 999904
+Condition exists (0:EAX=0 /\ 1:EAX=0) is validated
+Hash=2d53e83cd627ba17ab11c875525e078b
+Observation SB Sometimes 96 999904
+Time SB 0.11
+
+
+
+*/
+func main() {
+
+}
---
+++ advanced/reordering/memory/memory_reordering.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+
+https://cch123.github.io/ooo/
+
+从 Memory Reordering 说起
+
+下面这段代码会有怎样的输出
+var x, y int
+go func() {
+    x = 1 // A1
+    fmt.Print("y:", y, " ") // A2
+}()
+go func() {
+    y = 1                   // B1
+    fmt.Print("x:", x, " ") // B2
+}()
+
+显而易见的几种结果:
+y:0 x:1
+x:0 y:1
+x:1 y:1
+y:1 x:1
+令人意外的结果
+x:0 y:0
+y:0 x:0
+
+这种令人意外的结果被称为内存重排： Memory Reordering
+
+什么是内存重排
+
+内存重排指的是内存的读/写指令重排。  — Xargin
+
+软件或硬件系统可以根据其对代码的分析结果,一定程度上打乱代码的执行顺序，以达到其不可告人的目的。
+
+为什么会发生内存重排？
+
+1、编译器重排
+
+2、CPU重排
+
+
+
+*/
+func main() {
+	var wg sync.WaitGroup
+	for {
+		var x, y int
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			x = 1
+			fmt.Print("y:", y, " ")
+		}()
+
+		go func() {
+			defer wg.Done()
+			y = 1
+			fmt.Print("x:", x, " ")
+		}()
+		fmt.Println()
+		wg.Wait()
+	}
+}
