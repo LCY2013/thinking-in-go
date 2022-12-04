e9f09489503121923a7e6ce2156329e786afefa7
fufeng <15281205719@163.com>
2022-12-04 20:05:40 +0800

    feat: reflect field by index or by name, add benchmark compare

--- middleware/concurrency/reflect/fields_test.go
+++ middleware/concurrency/reflect/fields_test.go
@@ -129,3 +129,51 @@ func Test_SetField(t *testing.T) {
 		})
 	}
 }
+
+// TestLocalVariable 测试值是否可以通过反射被修改
+func TestLocalVariable(t *testing.T) {
+	i := 1
+	val := reflect.ValueOf(i)
+	ptrVal := reflect.ValueOf(&i)
+	t.Log(val.CanSet())           // false
+	t.Log(ptrVal.CanSet())        // false
+	t.Log(ptrVal.Elem().CanSet()) // true
+	ptrVal.Elem().Set(reflect.ValueOf(2))
+	t.Log(i) // 2
+}
+
+// BenchmarkFieldValueOfIndexOrName 对比字段通过索引查询和通过名称查询的性能差异
+// index 性能比 name 好很多
+// go test -bench=. -benchmem -benchtime=3s -run=none
+// run=none 忽略输出
+func BenchmarkFieldValueOfIndexOrName(b *testing.B) {
+	user := types.User{}
+	val := reflect.ValueOf(user)
+	ptrVal := reflect.ValueOf(&user).Elem()
+
+	b.Run("val by index", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			// 获取随机其中一个字段信息
+			_ = val.Field(1)
+		}
+	})
+	b.Run("ptrVal by index", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			// 获取随机其中一个字段信息
+			_ = ptrVal.Field(1)
+		}
+	})
+
+	b.Run("val by name", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			// 获取随机其中一个字段信息
+			_ = val.FieldByName("Name")
+		}
+	})
+	b.Run("ptrVal by name", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			// 获取随机其中一个字段信息
+			_ = ptrVal.FieldByName("Name")
+		}
+	})
+}
