97315247f639daf50724ccec37c6be9e9b884902
fufeng <15281205719@163.com>
2023-01-02 17:21:39 +0800

    feat: go template

---
+++ middleware/util/path.go
@@ -0,0 +1,61 @@
+package util
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func GetCurrentPath() (string, error) {
+	file, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	path, err := filepath.Abs(file)
+	if err != nil {
+		return "", err
+	}
+	i := strings.LastIndex(path, "/")
+	if i < 0 {
+		i = strings.LastIndex(path, "\\")
+	}
+	if i < 0 {
+		return "", errors.New(`error: Can't find "/" or "\"`)
+	}
+	return string(path[0 : i+1]), nil
+}
+
+func GetAllCurrentDir() []string {
+	var dirs []string
+	pwd, _ := os.Getwd()
+
+	//获取当前目录下的所有文件或目录信息
+	filepath.Walk(pwd, func(path string, info os.FileInfo, err error) error {
+		//fmt.Println(path) //打印path信息
+		//fmt.Println(info.Name()) //打印文件或目录名
+		dirs = append(dirs, path)
+		return nil
+	})
+	return dirs
+}
+
+func GetCurrentDir() []string {
+	var dirs []string
+	pwd, _ := os.Getwd()
+
+	//获取当前目录下的文件或目录名(包含路径)
+	filepathNames, err := filepath.Glob(filepath.Join(pwd, "*"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := range filepathNames {
+		//fmt.Println(filepathNames[i]) //打印path
+		dirs = append(dirs, filepathNames[i])
+	}
+
+	return dirs
+}
