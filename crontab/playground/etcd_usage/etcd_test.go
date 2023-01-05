22eb450ea1bc119fc5381ca6d73ae931f37e1060
fufeng <15281205719@163.com>
2023-01-05 22:24:29 +0800

    feat: go etcd lease

--- crontab/playground/etcd_usage/etcd_test.go
+++ crontab/playground/etcd_usage/etcd_test.go
@@ -30,6 +30,7 @@ func TestEtcd(t *testing.T) {
 	_ = client
 }
 
+// TestEtcdPut etcd put
 func (s *etcdTestSuite) TestEtcdPut() {
 	var (
 		kv          clientv3.KV
@@ -51,6 +52,7 @@ func (s *etcdTestSuite) TestEtcdPut() {
 	}
 }
 
+// TestEtcdGet etcd get
 func (s *etcdTestSuite) TestEtcdGet() {
 	var (
 		kv          clientv3.KV
@@ -93,6 +95,7 @@ func (s *etcdTestSuite) TestEtcdGet() {
 	s.T().Logf("With From Kvs: %v \n", getResponse.Kvs)
 }
 
+// TestEtcdGetPrefix etcd get prefix
 func (s *etcdTestSuite) TestEtcdGetPrefix() {
 	var (
 		kv          clientv3.KV
@@ -112,6 +115,7 @@ func (s *etcdTestSuite) TestEtcdGetPrefix() {
 	s.T().Log("----------------------------")
 }
 
+// TestEtcdDelete etcd删除机制
 func (s *etcdTestSuite) TestEtcdDelete() {
 	var (
 		kv      clientv3.KV
@@ -144,5 +148,84 @@ func (s *etcdTestSuite) TestEtcdDelete() {
 		for idx, kvpair = range delResp.PrevKvs {
 			s.T().Logf("Delete [%d] kv: key-%s , value-%s \n", idx, kvpair.Key, kvpair.Value)
 		}
+	}
+}
+
+// TestEtcdLease etcd 租约机制测试
+func (s *etcdTestSuite) TestEtcdLease() {
+	var (
+		kv                     clientv3.KV
+		err                    error
+		lease                  clientv3.Lease
+		leaseId                clientv3.LeaseID
+		leaseGrantResp         *clientv3.LeaseGrantResponse
+		putResp                *clientv3.PutResponse
+		getResp                *clientv3.GetResponse
+		keepRespChan           <-chan *clientv3.LeaseKeepAliveResponse
+		leaseKeepAliveResponse *clientv3.LeaseKeepAliveResponse
+	)
+
+	// 申请一个租约(lease)
+	lease = clientv3.NewLease(s.client)
+
+	// 申请一个10s的租约
+	if leaseGrantResp, err = lease.Grant(context.TODO(), 10); err != nil {
+		s.T().Error(err)
+		return
+	}
+
+	// 拿到租约ID
+	leaseId = leaseGrantResp.ID
+
+	// 5秒后取消自动续租
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	// 总共续租有15s的生命周期
+
+	// 自动续租
+	if keepRespChan, err = lease.KeepAlive(ctx, leaseId); err != nil {
+		s.T().Error(err)
+		return
+	}
+
+	// 处理续租应答
+	go func() {
+		for {
+			select {
+			case leaseKeepAliveResponse = <-keepRespChan:
+				if leaseKeepAliveResponse == nil {
+					s.T().Log("租约失效")
+					goto END
+				}
+				// 每秒会续租一次，所以就会收到一次应答
+				s.T().Logf("收到自动应答: %d", leaseKeepAliveResponse.ID)
+			}
+		}
+	END:
+	}()
+
+	// 获取kv API子集
+	kv = clientv3.NewKV(s.client)
+
+	// PUT 一个kv， 与租约关联，然后才能实现10s后自动过期
+	if putResp, err = kv.Put(context.TODO(), "/cron/lock/job1", "", clientv3.WithLease(leaseId)); err != nil {
+		s.T().Error(err)
+		return
+	}
+
+	s.T().Log("lock successful: ", putResp.Header.Revision)
+
+	// 定时查看key是否过期
+	for {
+		if getResp, err = kv.Get(context.TODO(), "/cron/lock/job1"); err != nil {
+			s.T().Error(err)
+			return
+		}
+		if getResp.Count == 0 {
+			s.T().Logf("%s 已过期", "/cron/lock/job1")
+			break
+		}
+		time.Sleep(2 * time.Second)
+		s.T().Logf("未过期: %v", getResp.Kvs)
 	}
 }
