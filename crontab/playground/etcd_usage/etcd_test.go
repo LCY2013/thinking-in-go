6847b33440783a0e021b07777e65f8cc9b3712cc
fufeng <15281205719@163.com>
2023-01-05 21:38:46 +0800

    feat: go etcd get

--- crontab/playground/etcd_usage/etcd_test.go
+++ crontab/playground/etcd_usage/etcd_test.go
@@ -29,7 +29,7 @@ func TestEtcd(t *testing.T) {
 	_ = client
 }
 
-func (s *etcdTestSuite) TestEtcdCURD() {
+func (s *etcdTestSuite) TestEtcdPut() {
 	var (
 		kv          clientv3.KV
 		putResponse *clientv3.PutResponse
@@ -47,4 +47,46 @@ func (s *etcdTestSuite) TestEtcdCURD() {
 	if putResponse.PrevKv != nil {
 		s.T().Logf("PrevKv: %s", putResponse.PrevKv.Value)
 	}
+}
+
+func (s *etcdTestSuite) TestEtcdGet() {
+	var (
+		kv          clientv3.KV
+		getResponse *clientv3.GetResponse
+		err         error
+	)
+
+	// 用于读写etcd健值对
+	kv = clientv3.NewKV(s.client)
+
+	if getResponse, err = kv.Get(context.TODO(), "/cron/jobs/job1"); err != nil {
+		s.T().Error(err)
+		return
+	}
+	s.T().Logf("Kvs: %v", getResponse.Kvs)
+	s.T().Logf("Revision: %v \n", getResponse.Header.Revision)
+
+	s.T().Log("----------------------------")
+	// 只返回数量
+	if getResponse, err = kv.Get(context.TODO(), "/cron/jobs/job1", clientv3.WithCountOnly()); err != nil {
+		s.T().Error(err)
+		return
+	}
+	s.T().Logf("With Count: %v \n", getResponse.Count)
+
+	s.T().Log("----------------------------")
+	// 返回某个前缀的数据
+	if getResponse, err = kv.Get(context.TODO(), "/cron/jobs", clientv3.WithPrefix()); err != nil {
+		s.T().Error(err)
+		return
+	}
+	s.T().Logf("With Prefix Kvs: %v \n", getResponse.Kvs)
+
+	s.T().Log("----------------------------")
+	// 返回某个前缀的数据
+	if getResponse, err = kv.Get(context.TODO(), "/cron/jobs", clientv3.WithFromKey()); err != nil {
+		s.T().Error(err)
+		return
+	}
+	s.T().Logf("With From Kvs: %v \n", getResponse.Kvs)
 }
