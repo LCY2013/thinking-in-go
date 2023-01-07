10c4a2e869a72b03236fd928893e174fb4bf8808
fufeng <15281205719@163.com>
2023-01-07 16:38:52 +0800

    feat: go mongodb find

--- crontab/playground/mongodb_usage/mongo_test.go
+++ crontab/playground/mongodb_usage/mongo_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
 )
 
@@ -35,6 +36,11 @@ type LogRecord struct {
 type TimePoint struct {
 	StartTime int64 `json:"startTime" bson:"startTime"`
 	EndTime   int64 `json:"endTime" bson:"endTime"`
+}
+
+// FindByJobName LogRecord filter by job name
+type FindByJobName struct {
+	JobName string `json:"jobName" bson:"jobName"` // JobName赋值为job0
 }
 
 // TestMongoDBInsert mongodb insert operation
@@ -76,4 +82,58 @@ func (m *mongoDbTestSuite) TestMongoDBInsert() {
 	// _id: 默认生成一个全局唯一的ID，ObjectID: 12字节的二进制
 	docId = insertResp.InsertedID.(primitive.ObjectID)
 	m.T().Logf("自增ID: %s", docId.Hex())
+}
+
+// TestMongoDBFind mongo find records
+func (m *mongoDbTestSuite) TestMongoDBFind() {
+	var (
+		database   *mongo.Database
+		collection *mongo.Collection
+
+		logRecord *LogRecord
+		cond      *FindByJobName
+		cursor    *mongo.Cursor
+
+		err error
+	)
+
+	// 选择数据库 my_db
+	database = m.client.Database("cron")
+
+	// 选择collection
+	collection = database.Collection("logs")
+
+	// 按照jobName 字段过滤，找出jobName=job0的记录
+	cond = &FindByJobName{
+		JobName: "job0",
+	}
+
+	// 查询(过滤 + 翻页)
+	if cursor, err = collection.Find(context.TODO(), cond, options.Find().SetSkip(0).SetLimit(2)); err != nil {
+		m.T().Error(err)
+		return
+	}
+
+	// 释放游标
+	defer func(cursor *mongo.Cursor, ctx context.Context) {
+		err = cursor.Close(ctx)
+		if err != nil {
+			m.T().Error(err)
+			return
+		}
+	}(cursor, context.TODO())
+
+	// 遍历结果集
+	for cursor.Next(context.TODO()) {
+		logRecord = &LogRecord{}
+
+		// 反序列化
+		if err = cursor.Decode(logRecord); err != nil {
+			m.T().Error(err)
+			return
+		}
+
+		// 把日志行打印出来
+		m.T().Logf("record: %+v", logRecord)
+	}
 }
