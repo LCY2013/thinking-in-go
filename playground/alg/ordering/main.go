b98ec38ab947e39a0b3e753763a39a21a32bfada
扶风 <fufeng@staruniongame.com>
2023-10-18 17:20:38 +0800

    Feat: Algorithms and Data Structures

--- playground/alg/ordering/main.go
+++ playground/alg/ordering/main.go
@@ -3,15 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	orderArr := []int{1, 3, 2, 5, 4, 8, 9, 1, 3, 10, 50, 22, 42, 12}
+	orderArr := []int{1, 3, 2, 5, 4, 8, 9, 10000, 100000, 1, 3, 10, 50, 80, 100, 22, 42, 12}
 	// 选择排序
 	//selectionSort(orderArr)
 	// 冒泡排序
 	//bubbleSort(orderArr)
 	// 插入排序
 	//insertionSort(orderArr)
+	// 希尔排序
+	shellSort(orderArr)
+	// 快速排序
+	quickSort(orderArr, 0, len(orderArr)-1)
 
 	fmt.Println(orderArr)
+}
+
+// quickSort 快速排序
+func quickSort(arr []int, start, end int) {
+
+}
+
+// shellSort 希尔排序
+func shellSort(arr []int) {
+	gap := 1
+	for len(arr)/3 > gap {
+		gap = 3*gap + 1
+	}
+
+	for ; gap > 0; gap /= 3 {
+		for i := gap; i < len(arr); i++ {
+			base := arr[i]
+			j := i
+			for ; j >= gap && arr[j-gap] > base; j -= gap {
+				arr[j] = arr[j-gap]
+			}
+			arr[j] = base
+		}
+	}
 }
 
 // insertionSort 插入排序
@@ -56,17 +84,3 @@ func selectionSort(arr []int) {
 		arr[min] = tmp
 	}
 }
-
-/*
-for i := 1; i < len(arr); i++ {
-		// 找到基准点
-		base := arr[i]
-		// 找到排序区域的最后一个位置
-		j := i - 1
-		for ; j > 0 && arr[j] > base; j-- {
-			arr[j+1] = arr[j]
-		}
-		// 将base插入正确位置
-		arr[j+1] = base
-	}
-*/
