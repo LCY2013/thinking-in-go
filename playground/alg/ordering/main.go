140c83466f94910b964d345e213c8e131adc86ba
扶风 <fufeng@staruniongame.com>
2023-10-18 18:55:14 +0800

    Feat: Algorithms and Data Structures

--- playground/alg/ordering/main.go
+++ playground/alg/ordering/main.go
@@ -1,26 +1,116 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func runtime(f func()) {
+	now := time.Now().UnixMilli()
+	f()
+	fmt.Printf("speed time: %d\n", time.Now().UnixMilli()-now)
+}
 
 func main() {
-	orderArr := []int{1, 3, 2, 5, 4, 8, 9, 10000, 100000, 1, 3, 10, 50, 80, 100, 22, 42, 12}
-	// 选择排序
-	//selectionSort(orderArr)
-	// 冒泡排序
-	//bubbleSort(orderArr)
-	// 插入排序
-	//insertionSort(orderArr)
-	// 希尔排序
-	shellSort(orderArr)
-	// 快速排序
-	quickSort(orderArr, 0, len(orderArr)-1)
+	length := 1000000000
+	orderArr := make([]int, length)
+	for i := 0; i < length; i++ {
+		orderArr[i] = rand.Intn(length)
+	}
+	//orderArr := []int{1, 3, 2, 5, 4, 8, 9, 10000, 100000, 1, 3, 10, 50, 80, 100, 22, 42, 12}
+	runtime(
+		func() {
+			// 选择排序
+			//selectionSort(orderArr)
+			// 冒泡排序
+			//bubbleSort(orderArr)
+			// 插入排序
+			//insertionSort(orderArr)
+			// 希尔排序
+			//shellSort(orderArr)
+			// 快速排序
+			quickSort(orderArr, 0, len(orderArr)-1)
+			//quickSortTailCall(orderArr, 0, len(orderArr)-1)
+		})
 
-	fmt.Println(orderArr)
+	//fmt.Println(orderArr)
+}
+
+// quickSortTailCall 快拍尾递归优化
+func quickSortTailCall(arr []int, left, right int) {
+	for left < right {
+		pivot := partitionMedian(arr, left, right)
+		if pivot-left > right-pivot {
+			partitionMedian(arr, left, pivot-1)
+			left = left + 1
+		} else {
+			partitionMedian(arr, pivot+1, right)
+			right = right - 1
+		}
+	}
 }
 
 // quickSort 快速排序
-func quickSort(arr []int, start, end int) {
+func quickSort(arr []int, left, right int) {
+	if left >= right {
+		return
+	}
+	// 获取新的基准点
+	//pivot := partition(arr, left, right)
+	pivot := partitionMedian(arr, left, right)
 
+	// 左
+	//quickSort(arr, left, pivot-1)
+	partitionMedian(arr, left, pivot-1)
+	//quickSort(arr, pivot+1, right)
+	partitionMedian(arr, pivot+1, right)
+}
+
+// partition 快速排序类-哨兵划分
+func partition(arr []int, left, right int) int {
+	i, j := left, right
+	for i < j {
+		for i < j && arr[j] >= arr[left] {
+			j--
+		}
+		for i < j && arr[i] <= arr[left] {
+			i++
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	arr[left], arr[i] = arr[i], arr[left]
+	return i
+}
+
+func medianThree(arr []int, left, mid, right int) int {
+	// 异或规则为 0 ^ 0 = 1 ^ 1 = 0, 0 ^ 1 = 1 ^ 0 = 1
+	if (arr[left] < arr[mid]^arr[left]) != (arr[left] < arr[right]) {
+		return left
+	} else if (arr[mid] < arr[left]) != (arr[mid] < arr[right]) {
+		return mid
+	}
+	return right
+}
+
+// partitionMedian 快速排序类-哨兵划分 三数取中值
+func partitionMedian(arr []int, left, right int) int {
+	i, j := left, right
+
+	mid := medianThree(arr, left, (left+right)/2, right)
+	arr[left], arr[mid] = arr[mid], arr[left]
+
+	for i < j {
+		for i < j && arr[j] >= arr[left] {
+			j--
+		}
+		for i < j && arr[i] <= arr[left] {
+			i++
+		}
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	arr[left], arr[i] = arr[i], arr[left]
+	return i
 }
 
 // shellSort 希尔排序
